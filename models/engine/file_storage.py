ce55e62fe6ac413c75648012b41e1334db2819a5
Somed-1 <abbasovmaqa159@gmail.com>
2024-03-26 18:41:05 +0400

    Update all methond and new methond delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, obj=None):
         """Returns a dictionary of models currently in storage"""
+        if obj:
+            temp = {}
+            for key, val in self.__objects.items():
+                if obj.__name__ in key:
+                    temp[key] = val
+            return temp
         return FileStorage.__objects
 
     def new(self, obj):
@@ -24,6 +30,14 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it’s inside"""
+        if obj:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.all():
+                del self.all()[key]
+            self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
