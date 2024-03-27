813a8821a9627714bc8dd1874e9baed1ea54ab83
Somed-1 <abbasovmaqa159@gmail.com>
2024-03-27 18:02:28 +0400

    user model upgrade

--- models/user.py
+++ models/user.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import Base, BaseModel
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
