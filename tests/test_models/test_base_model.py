5c929675e131c7f7df9f9e3a973cce7eddc0b848
Somed-1 <abbasovmaqa159@gmail.com>
2024-03-31 19:09:43 +0400

    Fix updated at test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -97,10 +97,7 @@ class test_basemodel(unittest.TestCase):
     def test_updated_at(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
-        n = new.to_dict()
-        new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
+        self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_uuid(self):
         """
