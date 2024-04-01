caeb9ea25adba981ca2900f428a2cfebe2de6cbd
Somed-1 <abbasovmaqa159@gmail.com>
2024-04-01 13:03:15 +0400

    Fix base model tests for db storage

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ """
+import models
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -57,6 +59,8 @@ class test_basemodel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = BaseModel(**copy)
 
+    @unittest.skipIf(type(models.storage) is DBStorage,
+                 "Testing FileStorage")
     def test_save(self):
         """ Testing save """
         i = self.value()
@@ -150,63 +154,65 @@ class Test_docstrings(unittest.TestCase):
         self.obj_members(BaseModel, inspect.isfunction)
 
 
- class TestBaseModel(unittest.TestCase):
-     """this will test the base model class x"""
+class TestBaseModel(unittest.TestCase):
+    """this will test the base model class x"""
 
-     @classmethod
-     def setUpClass(cls):
-         """setup for the test"""
-         cls.base = BaseModel()
-         cls.base.name = "Kev"
-         cls.base.num = 20
+    @classmethod
+    def setUpClass(cls):
+        """setup for the test"""
+        cls.base = BaseModel()
+        cls.base.name = "Kev"
+        cls.base.num = 20
 
-     @classmethod
-     def teardown(cls):
-         """at the end of the test this will tear it down"""
-         del cls.base
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.base
 
-     def tearDown(self):
-         """teardown"""
-         try:
-             os.remove("file.json")
-         except Exception:
-             pass
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-     def test_pep8_BaseModel(self):
-         """Testing for pep8"""
-         style = pycodestyle.StyleGuide(quiet=True)
-         p = style.check_files(['models/base_model.py'])
-         self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_pep8_BaseModel(self):
+        """Testing for pep8"""
+        style = pycodestyle.StyleGuide(quiet=True)
+        p = style.check_files(['models/base_model.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-     def test_checking_for_docstring_BaseModel(self):
-         """checking for docstrings"""
-         self.assertIsNotNone(BaseModel.__doc__)
-         self.assertIsNotNone(BaseModel.__init__.__doc__)
-         self.assertIsNotNone(BaseModel.__str__.__doc__)
-         self.assertIsNotNone(BaseModel.save.__doc__)
-         self.assertIsNotNone(BaseModel.to_dict.__doc__)
+    def test_checking_for_docstring_BaseModel(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
-     def test_method_BaseModel(self):
-         """checking if BaseModel has methods"""
-         self.assertTrue(hasattr(BaseModel, "__init__"))
-         self.assertTrue(hasattr(BaseModel, "save"))
-         self.assertTrue(hasattr(BaseModel, "to_dict"))
+    def test_method_BaseModel(self):
+        """checking if BaseModel has methods"""
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
 
-     def test_init_BaseModel(self):
-         """test if the base is an type BaseModel"""
-         self.assertTrue(isinstance(self.base, BaseModel))
+    def test_init_BaseModel(self):
+        """test if the base is an type BaseModel"""
+        self.assertTrue(isinstance(self.base, BaseModel))
 
-     def test_save_BaseModel(self):
-         """test if the save works"""
-         self.base.save()
-         self.assertNotEqual(self.base.created_at, self.base.updated_at)
+    @unittest.skipIf(type(models.storage) is DBStorage,
+                 "Testing FileStorage")
+    def test_save_BaseModel(self):
+        """test if the save works"""
+        self.base.save()
+        self.assertNotEqual(self.base.created_at, self.base.updated_at)
 
-     def test_to_dict_BaseModel(self):
-         """test if dictionary works"""
-         base_dict = self.base.to_dict()
-         self.assertEqual(self.base.__class__.__name__, 'BaseModel')
-         self.assertIsInstance(base_dict['created_at'], str)
-         self.assertIsInstance(base_dict['updated_at'], str)
+    def test_to_dict_BaseModel(self):
+        """test if dictionary works"""
+        base_dict = self.base.to_dict()
+        self.assertEqual(self.base.__class__.__name__, 'BaseModel')
+        self.assertIsInstance(base_dict['created_at'], str)
+        self.assertIsInstance(base_dict['updated_at'], str)
 
 
 if __name__ == "__main__":
