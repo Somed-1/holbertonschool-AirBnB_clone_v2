07d80eed585ca9ef4cac942dbf3f1f32a6bcaed5
Somed-1 <abbasovmaqa159@gmail.com>
2024-04-21 11:59:19 +0400

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Task 0"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_language(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>")
+def python_language(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
